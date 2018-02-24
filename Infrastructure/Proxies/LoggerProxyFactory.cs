0c70c3a5d4e00479e2519219de5935bfdf40acfc
Emil Heineking <ebheineking@gmail.com>
2018-02-24 10:37:47 -0500

    Updated logger message templates to match Serilog conventions

--- Infrastructure/Proxies/LoggerProxyFactory.cs
+++ Infrastructure/Proxies/LoggerProxyFactory.cs
@@ -12,9 +12,9 @@ namespace Infrastructure.Proxies {
       object decorated = DispatchProxy.Create<T, LoggerProxy<T>>();
       var proxy = ((LoggerProxy<T>)decorated);
       proxy.Decorate(implementation);
-      proxy.OnBeforeExecute += (t, e) => _logger.Information("Before Executing {methodName} from {declaringTypeFullName}", e.Name, e.DeclaringType.FullName);
-      proxy.OnAfterExecute += (t, e) => _logger.Information("After Executing {methodName} from {declaringTypeFullName}", e.Name, e.DeclaringType.FullName);
-      proxy.OnErrorExecute += (t, e) => _logger.Error(e.Exception, "Error Executing {methodName} from {declaringTypeFullName}", e.MethodInfo.Name, e.MethodInfo.DeclaringType.FullName);
+      proxy.OnBeforeExecute += (t, e) => _logger.Information("Before Executing {MethodName} from {DeclaringTypeFullName}", e.Name, e.DeclaringType.FullName);
+      proxy.OnAfterExecute += (t, e) => _logger.Information("After Executing {MethodName} from {DeclaringTypeFullName}", e.Name, e.DeclaringType.FullName);
+      proxy.OnErrorExecute += (t, e) => _logger.Error(e.Exception, "Error Executing {MethodName} from {DeclaringTypeFullName}", e.MethodInfo.Name, e.MethodInfo.DeclaringType.FullName);
       return (T)decorated;
     }
   }
